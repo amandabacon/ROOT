e692c175a34e024a70a083211b67bd5f3c2d0a50
amandabacon <ajbacon@sas.upenn.edu>
2022-06-29 10:52:45 -0400

    Templates for older dark box DF work

--- dichroic_dark_box/refl_graph.cpp
+++ dichroic_dark_box/refl_graph.cpp
@@ -7,35 +7,30 @@ using namespace std;
 int refl_graph(){
     TCanvas *c1 = new TCanvas("c1","Simple Graph",700,500); // width & height
 
-    const Int_t n = 12; // number of incidence angles; higher angles graze PMT
+    const Int_t n = 12; // remove 75 and 80 degrees due to grazing
 
-    Float_t x[n] = {15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0}; // array indexed by n; no refl less than 15 degrees
+    Float_t x[n] = {15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0};
 
-// Arrays of percent reflections for each LED indexed by n
-// yf5nr = y variable, f == first data point, 5 == 5 deg increments, n == n, r == reflection
-// ys5nr = same, but s == second time through
-// yt5nr = same, but t == third time through
+// 385nm
+    Float_t yf5nr_stable_385[n] = {};
 
-// 385nm LED
-    Float_t yf5nr_stable_385[n] = {24.7228033055,48.1608056469,74.9648071216,89.1680132701,90.1186694421,93.2165840513,96.4574034509,97.7328846349,97.5158785359,95.4261697024,92.8088902731,86.8875780757};
+// 405nm
+    Float_t yf5nr_stable_405[n] = {};
 
-// 405nm LED
-    Float_t yf5nr_stable_405[n] = {30.17660141,71.3677228,86.30467723,95.30069886,100.1656301,93.21657486,97.60564812,100.3316519,99.6925156,87.54272385,96.68762371,91.72794474};
+// 450nm
+    Float_t yf5nr_stable_450[n] = {};
 
-// 450nm LED
-    Float_t yf5nr_stable_450[n] = {49.2120381949,75.3859982815,90.702144858,94.7125532854,94.3947794088,88.7772924539,87.468723402,72.6646499223,60.8359650171,50.850088325,39.6964587563,24.3866885717};
+// 505nm
+    Float_t yt5nr_stable_505[n] = {};
 
-// 505nm LED
-    Float_t yt5nr_stable_505[n] = {17.2148651494,19.8569949445,19.0235396277,15.5272120553,12.4472005159,10.0756384746,9.1520366306,9.1232655377,9.6273783139,11.5777481495,14.0407561947,16.8939727671};
+// 525nm
+    Float_t yf5nr_stable_525[n] = {};
 
-// 525nm LED
-    Float_t yf5nr_stable_525[n] = {1.3088549838,6.5571581444,7.2843255179,7.5421605025,7.7376638041,8.0052576036,8.3447959043,8.8282703537,9.5597308274,10.1171225618,11.7977008534,14.0171765016};
+// 555nm
+    Float_t ys5nr_stable_555[n] = {};
 
-// 555nm LED
-    Float_t ys5nr_stable_555[n] = {1.7585300943,3.8949187137,4.8540163702,5.2116676832,5.5073532524,5.58343816,5.8783599227,6.3804411879,7.9068821817,8.7223092869,9.0503907612,4.9662651436};
-
-// 590nm LED
-    Float_t ys5nr_stable_590[n] = {2.0719838891,3.6253165076,4.918945339,4.6444271162,4.9356233645,5.4665827259,5.9528114775,7.1487447702,7.5440064319,8.5496935002,11.0662898772,12.883149515};
+// 590nm
+    Float_t ys5nr_stable_590[n] = {};
 
     c1->SetGrid();
 
@@ -92,13 +87,13 @@ int refl_graph(){
     c1->Update();
 
     TLegend *legend = new TLegend(0.1,0.7,0.3,0.9); // L,B,R,T 
-    legend->AddEntry(refl_385, "385nm October 25"); // light b/v
-    legend->AddEntry(refl_405, "405nm October 10"); // violet
-    legend->AddEntry(refl_450, "450nm September 6"); // blue
-    legend->AddEntry(refl_505, "505nm September 12"); // azure
-    legend->AddEntry(refl_525, "525nm September 12"); // green
-    legend->AddEntry(refl_555, "555nm September 5"); // green-yellow
-    legend->AddEntry(refl_590, "590nm September 20"); // yellow
+    legend->AddEntry(refl_385, "385nm"); // light b/v
+    legend->AddEntry(refl_405, "405nm"); // violet
+    legend->AddEntry(refl_450, "450nm"); // blue
+    legend->AddEntry(refl_505, "505nm"); // azure
+    legend->AddEntry(refl_525, "525nm"); // green
+    legend->AddEntry(refl_555, "555nm"); // green-yellow
+    legend->AddEntry(refl_590, "590nm"); // yellow
     legend->SetTextFont(132);
     legend->SetFillColor(0);
     legend->SetFillStyle(0);
--- dichroic_dark_box/sum_graph.cpp
+++ dichroic_dark_box/sum_graph.cpp
@@ -7,32 +7,30 @@ using namespace std;
 int sum_graph(){
     TCanvas *c1 = new TCanvas("c1","Simple Graph",700,500); // width & height
 
-    const Int_t n = 12; // number of incidence angles; higher angles graze PMT
+    const Int_t n = 12; // remove 75 and 80 degrees due to grazing
 
-    Float_t x[n] = {15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0}; // array indexed by n; no refl less than 15 degrees
+    Float_t x[n] = {15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0};
 
-// Arrays of the sum of percent tranmission and reflection for each LED indexed by n
+// 385nm
+    Float_t sum_stable_385[n] = {}; // of %T & %R
 
-// 385nm LED
-    Float_t sum_stable_385[n] = {24.894017475,48.349859533,75.0472015673,89.2745926347,90.2153359665,93.3430846651,96.5698498798,97.9053927188,97.7749845301,95.7532928619,93.2289486175,87.6219329863}; // of %T & %R
+// 405nm
+    Float_t sum_stable_405[n] = {}; // of %T & %R
 
-// 405nm LED
-    Float_t sum_stable_405[n] = {30.22440505,71.40807399,86.33484315,95.34265538,100.2094686,93.29183638,97.71036203,100.5012179,100.2054681,88.15873659,98.67974449,96.95220674}; // of %T & %R
+// 450nm
+    Float_t sum_stable_450[n] = {}; // of %T & %R
 
-// 450nm LED
-    Float_t sum_stable_450[n] = {50.1426834484,76.5955225796,92.3436364227,97.3422148248,98.095283833,95.1668681055,100.9564078037,101.2030798944,100.7944823492,99.9246134327,95.41828509,84.867671574}; // Sept 06
+// 505nm
+    Float_t sum_stable_505[n] = {}; // of %T & %R
 
-// 505nm LED
-    Float_t sum_stable_505[n] = {93.5190189222,96.5570541898,98.4856735177,99.3814560466,97.6171602554,99.3720281928,97.2332210005,97.4051951938,97.6467881492,98.4218595858,98.9573498575,96.7056214983}; // of %T & %R
+// 525nm
+    Float_t sum_stable_525[n] = {}; // of %T & %R
 
-// 525nm LED
-    Float_t sum_stable_525[n] = {89.932678415,99.4845244049,96.1335068458,96.9486437021,97.3608772877,96.4730141835,96.4273141354,96.9404601806,97.7166331074,97.3482299574,97.5828844668,94.0481311696}; // of %T & %R
+// 555nm
+    Float_t sum_stable_555[n] = {}; // of %T & %R
 
-// 555nm LED
-    Float_t sum_stable_555[n] = {98.6908491598,97.9731851152,98.5186333438,99.4472944979,100.0131327096,99.9471360302,99.4617963381,98.8698753281,97.9417117771,97.225637164,92.0857938721,83.4556826633}; // of %T & %R
-
-// 590nm LED
-    Float_t sum_stable_590[n] = {92.8664073741,92.5002397619,95.3353885238,92.8658030906,92.7162740883,98.5001045924,98.8436061321,99.6326120089,96.229524696,92.0817643436,89.4681654463,79.916342619}; // of %T & %R
+// 590nm
+    Float_t sum_stable_590[n] = {}; // of %T & %R
 
     c1->SetGrid();
 
@@ -89,13 +87,13 @@ int sum_graph(){
     c1->Update();
 
     TLegend *legend = new TLegend(0.7,0.5,0.9,0.7); // L,B,R,T 
-    legend->AddEntry(sum_385, "385nm October 25"); // light b/v
-    legend->AddEntry(sum_405, "405nm October 10"); // violet
-    legend->AddEntry(sum_450, "450nm September 6"); // blue
-    legend->AddEntry(sum_505, "505nm September 12"); // azure
-    legend->AddEntry(sum_525, "525nm September 12"); // green
-    legend->AddEntry(sum_555, "555nm September 5"); // green-yellow
-    legend->AddEntry(sum_590, "590nm September 20"); // yellow
+    legend->AddEntry(sum_385, "385nm"); // light b/v
+    legend->AddEntry(sum_405, "405nm"); // violet
+    legend->AddEntry(sum_450, "450nm"); // blue
+    legend->AddEntry(sum_505, "505nm"); // azure
+    legend->AddEntry(sum_525, "525nm"); // green
+    legend->AddEntry(sum_555, "555nm"); // green-yellow
+    legend->AddEntry(sum_590, "590nm"); // yellow
     legend->SetTextFont(132);
     legend->SetFillColor(0);
     legend->SetFillStyle(0);
